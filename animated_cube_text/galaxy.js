6914910c6136871da387f50720423a87d8004da4
YUVRAJ CHAUHAN <113971849+YBchauhan-EthicalHK@users.noreply.github.com>
2023-03-09 15:31:35 +0530

    Add files via upload

---
+++ animated_cube_text/galaxy.js
@@ -0,0 +1,59 @@
+
+        function modify1(){
+        // alert();
+        document.getElementById('box1').style.fontSize="0.5em";
+        // document.getElementById('main1').style.color="blue";
+        }
+        function remo1(){
+        // alert();
+        document.getElementById('box1').style.fontSize="2em";
+        // document.getElementById('main1').style.color="blue";
+        }
+        function modify2(){
+        // alert();
+        document.getElementById('box2').style.fontSize="0.5em";
+        // document.getElementById('main1').style.color="blue";
+        }
+        function remo2(){
+        // alert();
+        document.getElementById('box2').style.fontSize="2em";
+        // document.getElementById('main1').style.color="blue";
+        }
+        function modify3(){
+        // alert();
+        document.getElementById('box3').style.fontSize="0.5em";
+        // document.getElementById('main1').style.color="blue";
+        }
+        function remo3(){
+        // alert();
+        document.getElementById('box3').style.fontSize="2em";
+        // document.getElementById('main1').style.color="blue";
+        }function modify4(){
+        // alert();
+        document.getElementById('box4').style.fontSize="0.5em";
+        // document.getElementById('main1').style.color="blue";
+        }
+        function remo4(){
+        // alert();
+        document.getElementById('box4').style.fontSize="2em";
+        // document.getElementById('main1').style.color="blue";
+        }function modify5(){
+        // alert();
+        document.getElementById('box5').style.fontSize="0.5em";
+        // document.getElementById('main1').style.color="blue";
+        }
+        function remo5(){
+        // alert();
+        document.getElementById('box5').style.fontSize="2em";
+        // document.getElementById('main1').style.color="blue";
+        }
+        function modify6(){
+        // alert();
+        document.getElementById('box6').style.fontSize="0.5em";
+        // document.getElementById('main1').style.color="blue";
+        }
+        function remo6(){
+        // alert();
+        document.getElementById('box6').style.fontSize="2em";
+        // document.getElementById('main1').style.color="blue";
+        }
